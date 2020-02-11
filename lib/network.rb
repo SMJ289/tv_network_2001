2e2ba84f8e2612ee623299c20efba9b59c299912
Steven Meyers <sjjmeyers@gmail.com>
2020-02-11 10:26:08 -0700

    Add add_show method

--- lib/network.rb
+++ lib/network.rb
@@ -6,4 +6,7 @@ class Network
     @shows = []
   end
 
+  def add_show(show)
+    @shows << show
+  end
 end
--- test/network_test.rb
+++ test/network_test.rb
@@ -15,6 +15,7 @@ class NetworkTest < Minitest::Test
     @leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
     @ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
     @parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [@leslie_knope, @ron_swanson])
+
   end
 
   def test_it_exist
@@ -33,16 +34,10 @@ class NetworkTest < Minitest::Test
     assert_equal [@knight_rider, @parks_and_rec], @nbc.shows
   end
 
+  
+
 end
 
-
-# pry(main)> nbc.add_show(knight_rider)
-#
-# pry(main)> nbc.add_show(parks_and_rec)
-#
-# pry(main)> nbc.shows
-# # => [#<Show:0x00007fe5f8398970...>, #<Show:0x00007fe5f88b0a20...>]
-#
 # pry(main)> nbc.main_characters
 # # => [#<Character:0x00007f98a4ba8dc8...>]
 #
