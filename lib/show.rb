16d4ed096a97107e9ea36a4b6f25a691cc3e76fa
Steven Meyers <sjjmeyers@gmail.com>
2020-02-11 09:26:07 -0700

    Add show class and test with setup

---
+++ lib/show.rb
@@ -0,0 +1,10 @@
+class Show
+
+
+  def initialize(show_info)
+    @show_info = show_info
+  end
+
+
+
+end
--- test/show_test.rb
+++ test/show_test.rb
@@ -1,27 +1,41 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/character.rb'
 require './lib/show.rb'
 
 class ShowTest < Minitest::Test
+
+  def setup
+    @kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    @michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    @knight_rider = Show.new("Knight Rider", "Glen Larson", [@michael_knight, @kitt])
+  end
+
+  def test_it_exists
+
+    assert_instance_of Show, show
+  end
 
 
 
 
 end
 
-
-# ruby
-# pry(main)> require './lib/character'
-# # => true
 #
-# pry(main)> kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
-# # => #<Character:0x00007f98a4ba8dc8...>
+# pry(main)> knight_rider.name
+# # => "Knight Rider"
 #
-# pry(main)> kitt.name
-# # => "KITT"
+# pry(main)> knight_rider.creator
+# # => "Glen Larson"
 #
-# pry(main)> kitt.actor
-# # => "William Daniels"
+# pry(main)> knight_rider.characters
+# # => [#<Character:0x00007f8327213de0...>, #<Character:0x00007f8326ab57d8...>]
 #
-# pry(main)> kitt.salary
-# # => 1000000
+# pry(main)> knight_rider.total_salary
+# # => 2600000
+#
+# pry(main)> knight_rider.highest_paid_actor
+# # => "David Hasselhoff"
+#
+# pry(main)> knight_rider.actors
+# # => ["David Hasselhoff", "William Daniels"]
